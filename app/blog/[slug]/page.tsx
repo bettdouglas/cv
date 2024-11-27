af12562a19dbbfbec9211e012a5770d04ab6033e
Douglas Bett <bettdalpha@gmail.com>
2024-11-27 13:44:37 +0300

    make blogs static

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,15 @@ import { BlogPost } from '@/components/blog-post'
 import { blogPosts } from '@/data/blog-posts'
 import { notFound } from 'next/navigation'
 
+// This function tells Next.js exactly which blog posts to generate at build time
+export async function generateStaticParams() {
+  // We map through all blog posts to create an array of slug objects
+  // Each object must match the shape of the params your page expects
+  return blogPosts.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
 export default function BlogPostPage({ params }: { params: { slug: string } }) {
   const post = blogPosts.find(post => post.slug === params.slug)
 
